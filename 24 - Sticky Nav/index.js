04c2d83a2d26f2ba069682a089fc70e9ae55723e
sawfiz <dashu.up@gmail.com>
2022-09-14 17:20:03 +0800

    Clear up extra lines in 24 - Sticky Nav

--- 24 - Sticky Nav/index.js
+++ 24 - Sticky Nav/index.js
@@ -2,7 +2,6 @@ const navEl = document.querySelector("#main")
 topOfNav = navEl.offsetTop;
 
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY, topOfNav);
     if (window.scrollY >= topOfNav) {
         document.body.classList.add("fixed-nav")
         document.body.style.paddingTop = navEl.offsetHeight + "px"
@@ -10,5 +9,4 @@ window.addEventListener("scroll", () => {
         document.body.classList.remove("fixed-nav")
         document.body.style.paddingTop = 0
     }
-
 })
